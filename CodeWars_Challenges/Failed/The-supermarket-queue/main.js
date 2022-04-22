b18d2b4bf4f54f2d2d7c8bb6953b979cd9d081a7
Christopher Lopez <Chris.lopez174@gmail.com>
2022-04-22 18:13:15 -0400

    Daily Codewars challenge

--- CodeWars_Challenges/Failed/The-supermarket-queue/main.js
+++ CodeWars_Challenges/Failed/The-supermarket-queue/main.js
@@ -31,3 +31,20 @@ function queueTime(customers, n) {
     return totalTime
 }
 queueTime([1,2,3,4], 2)
+
+//Failed //
+
+// TOP SOLUTION //
+function queueTime(customers, registers) {
+    let arr = [];
+  
+    for(let i = 0; i < registers; i++)
+      arr[i] = 0;
+  
+    for(let i = 0; i < customers.length; i++) {
+      arr[0] += customers[i];
+      arr.sort((a, b) => a - b);
+    }
+  
+    return arr[arr.length - 1];
+  }
