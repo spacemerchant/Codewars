18b754ce2b36a143e5f669b780d0ed0f794d4761
Christopher Lopez <Chris.lopez174@gmail.com>
2022-04-25 18:51:15 -0400

    Daily Codewars challenge

---
+++ CodeWars_Challenges/Passed/Area-or-perimeter/main.js
@@ -0,0 +1,14 @@
+const areaOrPerimeter = function(l , w) {
+    return l === w ? l * w : 2*(l + w);
+};
+
+console.log(areaOrPerimeter(5,5))
+
+// Difficulty 1/5 // 
+
+
+// Top Solutions //
+
+const areaOrPerimetera = function(l , w) {
+    return l == w ? l*w : 2*(l + w)
+};
---
+++ CodeWars_Challenges/Passed/Multiply-two-numbers/main.js
@@ -0,0 +1,5 @@
+let multiply = (num1, num2) => num1 * num2
+
+
+//Difficulty -5/5
+
