35d02030a0d612a0d1141e5edfb2a02138254f8d
Christopher Lopez <christopherldevelopment@gmail.com>
2022-04-29 13:16:07 -0400

    Daily Codewars challenge

---
+++ CodeWars_Challenges/Passed/You're-a-square/main.js
@@ -0,0 +1,13 @@
+var isSquare = function(n){
+    return (Number.isInteger(Math.sqrt(n))) ? true : false;
+}
+
+
+//Difficulty 1/5 //
+
+
+// Top solutions //
+
+var isSquare = function(n){
+    return Number.isInteger(Math.sqrt(n));
+}
