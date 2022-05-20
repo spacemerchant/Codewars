e1c5520c0aa604667a6fbb08e16a25dd68afb05a
Christopher Lopez <Chris.lopez174@gmail.com>
2022-05-20 16:32:55 -0400

    Daily Codewars challenge

---
+++ CodeWars_Challenges/Passed/sort-by-string-length-(7)/main.js
@@ -0,0 +1,12 @@
+function sortByLength (array) {
+    return array.sort((a,b) => a.length - b.length)
+};
+sortByLength(["Beg", "Life", "I", "To"])
+
+//Difficulty 0/5 
+
+//TOP SOLUTION
+
+function sortByLength (array) {
+    return array.sort((a,b) => a.length - b.length);
+};
